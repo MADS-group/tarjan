a7418f855a016f62b0c7b0d3b0aea2d4bea8175b
AntoSave <kiraanto@hotmail.it>
2022-12-23 11:19:11 +0100

    Added a graph repesentation for CUDA

--- common/include/cuda_graph.h
+++ common/include/cuda_graph.h
@@ -15,6 +15,6 @@ typedef struct cuda_graph_t cuda_graph_t;
 cuda_graph_t *cuda_graph_load_from_file(char *);
 graph_t *cuda_graph_to_graph(cuda_graph_t *, int *);
 void cuda_graph_free(cuda_graph_t *);
-
+void cuda_graph_print_debug(cuda_graph_t *G);
 
 #endif
