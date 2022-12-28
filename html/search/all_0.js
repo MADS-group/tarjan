dceb4d252e19c4b1fdcaf45184eb0022fa0f3ccd
my-rice <dvdrsi2017@gmail.com>
2022-12-28 13:27:20 +0100

    ...

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['license_0',['LICENSE',['../md_LICENSE.html',1,'']]]
+  ['master_5fschedule_0',['master_schedule',['../mpi__logic_8c.html#afe4519dc9cac0f0c0e30f8718a357cad',1,'mpi_logic.c']]],
+  ['mpi_5flogic_2ec_1',['mpi_logic.c',['../mpi__logic_8c.html',1,'']]]
 ];
---
+++ html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mpi_5flogic_2ec_2',['mpi_logic.c',['../mpi__logic_8c.html',1,'']]]
+];
