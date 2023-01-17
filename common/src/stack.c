50ca4b54b23e58e824ac949fa7f50190f42b9ce9
Poggi28 <paolinosasa@libero.it>
2023-01-17 12:50:57 +0100

    Add copyright to all files

--- common/src/stack.c
+++
@@ -1,7 +0,0 @@
-/*Decidere se implementare con linked list o dynamic array a seconda degli algoritmi che useranno gli stack*/
-#include "stack.h"
-
-struct stack_t {
-
-};
-//????????????? Alla fine array e linkedlist possono essere usate sia come stack che non!!!
--- html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['master_5fschedule_31',['master_schedule',['../mpi__logic_8c.html#afe4519dc9cac0f0c0e30f8718a357cad',1,'mpi_logic.c']]]
-];
